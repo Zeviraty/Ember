effa34bee074e6d8070eae0bdc2b724333438ac1
zeviraty <zeviraty@gmail.com>
2026-03-06 13:43:52 +0100

    feat(gen): More map data

--- scripts/generate_files.py
+++ scripts/generate_files.py
@@ -1,7 +1,15 @@
 import os
 import shutil
 import sys
-from pprint import pp
+
+BST_REMAP = {
+    'MART': 'LOBBY',
+    'DOJO': 'GYM',
+    'MUSEUM': 'FACILITY',
+    'FOREST_GATE': 'GATE',
+    'REDS_HOUSE_1': 'REDS_HOUSE',
+    'REDS_HOUSE_2': 'REDS_HOUSE',
+}
 
 def gen() -> int:
     if shutil.which("git") in (None, ""):
@@ -20,9 +28,22 @@ def gen() -> int:
 
     HEADER_START = 'map_header '
     HEADER_END = 'end_map_header'
+    MAP_CONST = 'map_const'
     HEADER_FOLDER = 'data/pokered/data/maps/headers'
     CONNECTION = 'connection '
     os.mkdir("data/maps")
+
+    constants = open('data/pokered/constants/map_constants.asm','r').read()
+    SIZES = {}
+    for line in constants.splitlines():
+        if not line.strip().startswith(MAP_CONST):
+            continue
+        parts = []
+        for part in line[:line.index(';')].strip().split(','):
+            spl = part.split()
+            if spl[0] == MAP_CONST: spl.pop(0)
+            parts.append(spl[0])
+        SIZES[parts[0]] = {'x': parts[1], 'y': parts[2]}
 
     headers = os.listdir(HEADER_FOLDER)
     length = len(headers)
@@ -37,6 +58,7 @@ def gen() -> int:
             "South": 0,
             "West":  0
         }
+
         for line in open(HEADER_FOLDER+os.sep+header,'r').read().splitlines():
             if line.strip() == HEADER_END:
                 break
@@ -46,7 +68,9 @@ def gen() -> int:
                         .split(',')
                 data['ReadableName'] = parts[0]
                 data['ID'] = parts[1]
-                data['BlockSet'] = parts[2]
+                bst = parts[2]
+                bst = BST_REMAP.get(bst, bst)
+                data['BlockSet'] = bst
             elif line.strip().startswith(CONNECTION):
                 parts = line.strip()[len(CONNECTION):] \
                         .replace(', ',',') \
@@ -75,6 +99,12 @@ def gen() -> int:
             f.write(b'\x01') # list seperator
             f.write(str(data[dir]['offset']).encode())
             f.write(b'\x00') # Null byte seperator
+
+        f.write(str(SIZES[data['ID']]['x']).encode())
+        f.write(b'\x00')
+        f.write(str(SIZES[data['ID']]['y']).encode())
+        f.write(b'\x00')
+
         f.write(open(f'data/pokered/maps/{data["ReadableName"].removesuffix("Copy")}.blk','rb').read())
 
         sys.stdout.write(f'\33[2K\r')
@@ -82,7 +112,11 @@ def gen() -> int:
 
     sys.stdout.write('\n')
 
-    shutil.copytree('data/pokered/gfx/blocksets','data/bst')
+    bst_path = 'data/bst'
+    shutil.copytree('data/pokered/gfx/blocksets',bst_path)
+    shutil.copytree('data/pokered/gfx/tilesets','data/tilesets')
+    for file in os.listdir(bst_path):
+        shutil.move(bst_path+os.sep+file,bst_path+os.sep+file.removesuffix('.bst').upper())
  
     #shutil.rmtree("data/pokered")
     return 0
